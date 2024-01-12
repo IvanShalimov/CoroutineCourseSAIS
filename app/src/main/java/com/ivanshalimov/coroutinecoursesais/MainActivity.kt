ae0e115f1e955402fa1468cfd4e6fcc2c25fbb3a
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-01-12 15:58:38 +0400

    first practice

--- app/src/main/java/com/ivanshalimov/coroutinecoursesais/MainActivity.kt
+++ app/src/main/java/com/ivanshalimov/coroutinecoursesais/MainActivity.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import com.ivanshalimov.coroutinecoursesais.ui.theme.CoroutineCourseSAISTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     val scope = CoroutineScope(Job()) // the simplest creation of coroutine
+    private var formatter = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +45,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        scope.launch {
+        /*scope.launch {
             Log.d("Ivan","My first coroutine")
         }
         scope.launch {
@@ -54,28 +62,64 @@ class MainActivity : ComponentActivity() {
                 Log.d("Ivan","second this: $this")
             }
         }
-        Log.d("Ivan","top job: $job")
-
+        Log.d("Ivan","top job: $job")*/
     }
 
     override fun onDestroy() {
         super.onDestroy()
         scope.cancel()
     }
+
+    private fun onRun() {
+        log("onRun, start")
+        scope.launch {
+            log("coroutine start")
+            TimeUnit.MILLISECONDS.sleep(3000L)
+            log("coroutine end")
+        }
+        log("middle")
+        scope.launch {
+            log("coroutine1 start")
+            TimeUnit.MILLISECONDS.sleep(1500L)
+            log("coroutine1 end")
+        }
+        log("onRun, end")
+    }
+
+    private fun onCancel() {
+
+    }
+
+    private fun log(text: String) {
+        Log.d("TAG", "${formatter.format(Date())} $text [${Thread.currentThread().name}]")
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        /*Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )*/
+        Row {
+            Button(onClick = { onRun() }) {
+                Text("Run")
+            }
+            Button(onClick = { onCancel() }) {
+                Text("Cancel")
+            }
+        }
+    }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
+
+
+/*
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CoroutineCourseSAISTheme {
         Greeting("Android")
     }
-}
+}*/
